7ae4c7468e9d4f7f510ad29daa1426708baec71e
Ryan Truran <ryan.truran@gmail.com>
2017-12-17 07:59:23 -0600

    copied index.html to index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Truran Homepage</title>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,‌​100italic,300,300ita‌​lic,400italic,500,50‌​0italic,
+    700,700itali‌​c,900italic,900' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="style/home-page.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="item">
+      <a href="../owncloud/index.html"><img id="owncloud" src="image/OwnCloud-Logo-Red.png"></a>
+    </div>
+    <div class="item">
+      <a href="../kanboard">
+        <svg xmlns="http://www.w3.org/2000/svg"
+          width="100%" height="100%">
+          <text x="40%" y="50%" font-family="helvetica" fill = "red" stroke = "red" font-size="100" text-anchor="middle">
+            K
+          </text>
+          <text x="60%" y="50%" font-family="helvetica" fill = "white" stroke = "white" font-size="100" text-anchor="middle">
+            B
+          </text>
+        </svg>
+      </a>
+    </div>
+    <div class="item">
+      <a href="ryan/index.html">
+        <div>
+          <img id="about-me" src="../ryan/img/Profile-Picture-Cropped.png">
+          <h2>About Me</h2>
+        </div>
+      </a>
+    </div>
+  </div>
+
+</body>
+
+</html>
